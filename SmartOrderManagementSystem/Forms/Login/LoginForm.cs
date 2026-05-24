414de61725a559b2503dcb348b374349ef97f457
Lay Kimhoung <houngjk081@gmail.com>
2026-05-24 23:19:34 +0700

    Add project files.

---
+++ SmartOrderManagementSystem/Forms/Login/LoginForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartOrderManagementSystem.Forms.Admin;
+using SmartOrderManagementSystem.Forms.Staff;
+using SmartOrderManagementSystem.Forms.Customer;
+
+namespace SmartOrderManagementSystem.Forms.Login
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SmartOrderManagementSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartOrderManagementSystem.Forms.Login;
+using SmartOrderManagementSystem.Forms.Admin;
+using SmartOrderManagementSystem.Forms.Staff;
+using SmartOrderManagementSystem.Forms.Customer;
+
+namespace SmartOrderManagementSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}

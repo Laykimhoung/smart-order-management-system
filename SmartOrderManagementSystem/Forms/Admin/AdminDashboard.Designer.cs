6d9ae7c5239fea8decd71186a0cb86ab07bc52ad
Lay Kimhoung <houngjk081@gmail.com>
2026-05-27 17:16:23 +0700

    ok

--- SmartOrderManagementSystem/Forms/Admin/AdminDashboard.Designer.cs
+++ SmartOrderManagementSystem/Forms/Admin/AdminDashboard.Designer.cs
@@ -28,74 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.parrotForm1 = new ReaLTaiizor.Forms.ParrotForm();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panelTopbar = new System.Windows.Forms.Panel();
-            this.panelSidebar = new System.Windows.Forms.Panel();
-            this.panelMainContent = new System.Windows.Forms.Panel();
-            this.panelTopbar.SuspendLayout();
-            this.panelMainContent.SuspendLayout();
+            this.cyberButton1 = new ReaLTaiizor.Controls.CyberButton();
+            this.parrotForm1.WorkingArea.SuspendLayout();
+            this.parrotForm1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // parrotForm1
+            // 
+            this.parrotForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.parrotForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.parrotForm1.ExitApplication = true;
+            this.parrotForm1.FormStyle = ReaLTaiizor.Forms.ParrotForm.Style.MacOS;
+            this.parrotForm1.Location = new System.Drawing.Point(0, 0);
+            this.parrotForm1.MacOSForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.parrotForm1.MacOSLeftBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.parrotForm1.MacOSRightBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.parrotForm1.MacOSSeparatorColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(173)))), ((int)(((byte)(173)))));
+            this.parrotForm1.MaterialBackColor = System.Drawing.Color.DodgerBlue;
+            this.parrotForm1.MaterialForeColor = System.Drawing.Color.White;
+            this.parrotForm1.Name = "parrotForm1";
+            this.parrotForm1.ShowMaximize = true;
+            this.parrotForm1.ShowMinimize = true;
+            this.parrotForm1.Size = new System.Drawing.Size(1600, 900);
+            this.parrotForm1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            this.parrotForm1.TabIndex = 0;
+            this.parrotForm1.TitleText = "Parrot Form";
+            this.parrotForm1.UbuntuForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(210)))));
+            this.parrotForm1.UbuntuLeftBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(85)))), ((int)(((byte)(80)))));
+            this.parrotForm1.UbuntuRightBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(60)))));
+            // 
+            // parrotForm1.WorkingArea
+            // 
+            this.parrotForm1.WorkingArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.parrotForm1.WorkingArea.Controls.Add(this.panel1);
+            this.parrotForm1.WorkingArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.parrotForm1.WorkingArea.Location = new System.Drawing.Point(0, 39);
+            this.parrotForm1.WorkingArea.Name = "WorkingArea";
+            this.parrotForm1.WorkingArea.Size = new System.Drawing.Size(1600, 861);
+            this.parrotForm1.WorkingArea.TabIndex = 0;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(0, 76);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.cyberButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 206);
+            this.panel1.Size = new System.Drawing.Size(250, 861);
             this.panel1.TabIndex = 0;
             // 
-            // panelTopbar
+            // cyberButton1
             // 
-            this.panelTopbar.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.panelTopbar.Controls.Add(this.panel1);
-            this.panelTopbar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTopbar.Location = new System.Drawing.Point(0, 0);
-            this.panelTopbar.Name = "panelTopbar";
-            this.panelTopbar.Size = new System.Drawing.Size(1397, 75);
-            this.panelTopbar.TabIndex = 1;
-            // 
-            // panelSidebar
-            // 
-            this.panelSidebar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelSidebar.Location = new System.Drawing.Point(2, 2);
-            this.panelSidebar.Name = "panelSidebar";
-            this.panelSidebar.Size = new System.Drawing.Size(250, 777);
-            this.panelSidebar.TabIndex = 0;
-            // 
-            // panelMainContent
-            // 
-            this.panelMainContent.BackColor = System.Drawing.SystemColors.Info;
-            this.panelMainContent.Controls.Add(this.panelSidebar);
-            this.panelMainContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMainContent.Location = new System.Drawing.Point(0, 0);
-            this.panelMainContent.Name = "panelMainContent";
-            this.panelMainContent.Padding = new System.Windows.Forms.Padding(2);
-            this.panelMainContent.Size = new System.Drawing.Size(1397, 781);
-            this.panelMainContent.TabIndex = 2;
+            this.cyberButton1.Alpha = 20;
+            this.cyberButton1.BackColor = System.Drawing.Color.Transparent;
+            this.cyberButton1.Background = true;
+            this.cyberButton1.Background_WidthPen = 4F;
+            this.cyberButton1.BackgroundPen = true;
+            this.cyberButton1.ColorBackground = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(52)))), ((int)(((byte)(68)))));
+            this.cyberButton1.ColorBackground_1 = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(52)))), ((int)(((byte)(68)))));
+            this.cyberButton1.ColorBackground_2 = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.cyberButton1.ColorBackground_Pen = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
+            this.cyberButton1.ColorLighting = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
+            this.cyberButton1.ColorPen_1 = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(52)))), ((int)(((byte)(68)))));
+            this.cyberButton1.ColorPen_2 = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(63)))), ((int)(((byte)(86)))));
+            this.cyberButton1.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            this.cyberButton1.Effect_1 = true;
+            this.cyberButton1.Effect_1_ColorBackground = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
+            this.cyberButton1.Effect_1_Transparency = 25;
+            this.cyberButton1.Effect_2 = true;
+            this.cyberButton1.Effect_2_ColorBackground = System.Drawing.Color.White;
+            this.cyberButton1.Effect_2_Transparency = 20;
+            this.cyberButton1.Font = new System.Drawing.Font("Arial", 11F);
+            this.cyberButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.cyberButton1.Lighting = false;
+            this.cyberButton1.LinearGradient_Background = false;
+            this.cyberButton1.LinearGradientPen = false;
+            this.cyberButton1.Location = new System.Drawing.Point(12, 20);
+            this.cyberButton1.Name = "cyberButton1";
+            this.cyberButton1.PenWidth = 15;
+            this.cyberButton1.Rounding = true;
+            this.cyberButton1.RoundingInt = 70;
+            this.cyberButton1.Size = new System.Drawing.Size(205, 50);
+            this.cyberButton1.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.cyberButton1.TabIndex = 0;
+            this.cyberButton1.Tag = "Cyber";
+            this.cyberButton1.TextButton = "CyberButton";
+            this.cyberButton1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.cyberButton1.Timer_Effect_1 = 5;
+            this.cyberButton1.Timer_RGB = 300;
             // 
             // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1397, 781);
-            this.Controls.Add(this.panelTopbar);
-            this.Controls.Add(this.panelMainContent);
+            this.ClientSize = new System.Drawing.Size(1600, 900);
+            this.Controls.Add(this.parrotForm1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminDashboard";
             this.TransparencyKey = System.Drawing.Color.Fuchsia;
-            this.panelTopbar.ResumeLayout(false);
-            this.panelMainContent.ResumeLayout(false);
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.parrotForm1.WorkingArea.ResumeLayout(false);
+            this.parrotForm1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private ReaLTaiizor.Forms.ParrotForm parrotForm1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panelTopbar;
-        private System.Windows.Forms.Panel panelSidebar;
-        private System.Windows.Forms.Panel panelMainContent;
+        private ReaLTaiizor.Controls.CyberButton cyberButton1;
     }
 }

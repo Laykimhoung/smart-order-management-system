9f80435ae8334e987540b8ea6a48bbba583111f8
naingvannath269-maker <naingvannath269@gmail.com>
2026-05-28 10:50:05 +0700

    Retrive Data

--- SmartOrderManagementSystem/Forms/Customer/CustomerDashboard.cs
+++ SmartOrderManagementSystem/Forms/Customer/CustomerDashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SmartOrderManagementSystem.Forms.Customer
 {
@@ -15,6 +16,43 @@ namespace SmartOrderManagementSystem.Forms.Customer
         public CustomerDashboard()
         {
             InitializeComponent();
+        }
+        public DataTable GetProducts(string categoryName = "All")
+        {
+            DataTable productsTable = new DataTable();
+            string query = @"SELECT p.ProductID, p.ProductName, p.Price, p.StockQuantity, c.CategoryName, p.ProductImage 
+                FROM Products p
+                INNER JOIN Categories c ON p.CategoryID = c.CategoryID";
+
+            if(categoryName != "All")
+            {
+                query += " WHERE c.CategoryName = @CategoryName";
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (categoryName != "All")
+                    {
+                        cmd.Parameters.AddWithValue("@CategoryName", categoryName);
+                    }
+
+                    try
+                    {
+                        conn.Open();
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            adapter.Fill(productTable);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error fetching products: " + ex.Message);
+                    }
+                }
+            }
+            reeturn productsTable;
         }
     }
 }

a3a24b68d17d6b2b7730aaef38cffd1115c6a4d9
Lay Kimhoung <houngjk081@gmail.com>
2026-06-06 17:12:45 +0700

    text in Analysis

--- SmartOrderManagementSystem/Forms/Admin/OrderDetailsForm.Designer.cs
+++ SmartOrderManagementSystem/Forms/Admin/OrderDetailsForm.Designer.cs
@@ -215,7 +215,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(678, 717);
             this.Controls.Add(this.btnGenerateInvoice);
             this.Controls.Add(this.label5);

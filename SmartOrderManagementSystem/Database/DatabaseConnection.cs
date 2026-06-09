ffb501970e9e9e27021be9348ab1e152f18469d3
Lay Kimhoung <houngjk081@gmail.com>
2026-06-09 09:39:26 +0700

    connect database

--- SmartOrderManagementSystem/Database/DatabaseConnection.cs
+++ SmartOrderManagementSystem/Database/DatabaseConnection.cs
@@ -10,10 +10,9 @@ namespace SmartOrderManagementSystem.Database
 {
     public static class DatabaseConnection
     {
+        //Use your own local database connection string here
         private static readonly string connectionString =
-         @"Server=PROTECT-ME;Database=SmartOrderDB;User Id=sa;Password=sa;TrustServerCertificate=True;";
-
-
+         @"Server=MSI\MYSQLSERVER;Database=SmartOrderDB;User Id=sa;Password=sa;TrustServerCertificate=True;";
 
 
         public static SqlConnection GetConnection()
--- SmartOrderManagementSystem/Forms/Admin/AdminDashboard.cs
+++ SmartOrderManagementSystem/Forms/Admin/AdminDashboard.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SmartOrderManagementSystem.Database;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,7 +22,18 @@ namespace SmartOrderManagementSystem.Forms.Admin
 
         private void AdminDashboard_Load(object sender, EventArgs e)
         {
-          
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+                    MessageBox.Show("Connected!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private Form activeForm = null;
 

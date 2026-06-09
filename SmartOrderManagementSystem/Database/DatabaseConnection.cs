62f99d65a790d596c74544582dae32824c407bf7
SonitaMom <sonitamom9@gmail.com>
2026-06-09 13:50:11 +0700

    ta

--- SmartOrderManagementSystem/Database/DatabaseConnection.cs
+++ SmartOrderManagementSystem/Database/DatabaseConnection.cs
@@ -11,7 +11,7 @@ namespace SmartOrderManagementSystem.Database
     public static class DatabaseConnection
     {
         private static readonly string connectionString =
-         @"Server=PROTECT-ME;Database=SmartOrderDB;User Id=sa;Password=sa;TrustServerCertificate=True;";
+         @"Server=DESKTOP-V7K9TLS\SQL2019;Database=SmartOrderManagementDB;User Id=sa;Password=sa;TrustServerCertificate=True;";
 
 
 
--- SmartOrderManagementSystem/Forms/Staff/SettingForm.cs
+++ SmartOrderManagementSystem/Forms/Staff/SettingForm.cs
@@ -16,6 +16,7 @@ namespace SmartOrderManagementSystem.Forms.Staff
         public SettingForm()
         {
             InitializeComponent();
+           
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -26,26 +27,46 @@ namespace SmartOrderManagementSystem.Forms.Staff
         private void SettingForm_Load(object sender, EventArgs e)
         {
             // Add selecting sex
-            Sex_combobox.Items.AddRange(new string[] { "Male", "Female" });
+            //Sex_combobox.Items.AddRange(new string[] { "Male", "Female" });
+            Load_user_staffinfo();
         }
 
         // Load the staff information into the setting form
-        private void Laod_user_staffinfo()
+        // Declare the constructure of the query to get the staff information from the database
+        private int Staff_id;
+
+        public void Profile_Form(int StaffID)
         {
-            string query = @"SELECT u.UserID,u.Username,u.Fullname,u.Email,u.PhoneNumber,r.RoleID FROM Users
-                            LEFT JOIN Roles r ON u.RoleID = r.RoleID GROUP BY u.UserID,u.Username,u.Fullname,u.Email,u.PhoneNumber,r.RoleID";
+            InitializeComponent();
+            Staff_id = StaffID;
+        }
+        private void Load_user_staffinfo()
+        {
+            string query = @"SELECT UserID,Username,Fullname,Email,Phone,RoleID " + "FROM Users WHERE UserID = " + Staff_id;
+                            //LEFT JOIN (SELECT UserID FROM Users WHERE )Roles r ON u.RoleID = r.RoleID GROUP BY u.UserID,u.Username,u.Fullname,u.Email,u.PhoneNumber,r.RoleID";
             try
             {
                 DataTable dt = DatabaseConnection.ExecuteQuery(query);
                 if (dt.Rows.Count > 0)
                 {
                     DataRow row = dt.Rows[0];
+                    
+                    // Fill the data to each textbox
                     Userid_txt.Text = row["UserID"].ToString();
                     Fullname_txt.Text = row["Fullname"].ToString();
                     Email_txt.Text = row["Email"].ToString();
-                    Phonenumber_txt.Text = row["PhoneNumber"].ToString();
+                    Phonenumber_txt.Text = row["Phone"].ToString();
+                    Role_txt.Text = row["RoleID"].ToString();
+                    Sex_txt.Text = row["RoleID"].ToString();
+                    username_txt.Text = row["username"].ToString();
 
-
+                    // name under the photo in Profil
+                    Name_lbl.Text = row["Fullname"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No profile data found.", "Info",
+                       MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             catch (Exception ex)

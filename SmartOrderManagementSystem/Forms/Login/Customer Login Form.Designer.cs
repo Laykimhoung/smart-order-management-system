92f6b23027fff5c03d0ab5c9d6848d307ed20844
Prak Channarith <channarithbrak@gmail.com>
2026-06-11 14:07:24 +0700

    Code + a bit UI

--- SmartOrderManagementSystem/Forms/Login/Customer Login Form.Designer.cs
+++ SmartOrderManagementSystem/Forms/Login/Customer Login Form.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomerLoginForm));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnGoToCusForm = new System.Windows.Forms.Button();
             this.txtPhNum = new System.Windows.Forms.TextBox();
@@ -39,13 +41,18 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.btnGoToCusForm);
             this.panel1.Controls.Add(this.txtPhNum);
@@ -56,75 +63,108 @@
             this.panel1.Size = new System.Drawing.Size(587, 832);
             this.panel1.TabIndex = 0;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label7.Location = new System.Drawing.Point(20, 776);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(228, 44);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "© 2026 Jinghab Drink House\r\nSmart Order System";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Banner", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Turquoise;
+            this.label5.Location = new System.Drawing.Point(174, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(259, 68);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Quick Login";
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Sitka Banner", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Sitka Banner", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(80, 224);
+            this.label2.Location = new System.Drawing.Point(33, 335);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(381, 49);
+            this.label2.Size = new System.Drawing.Size(478, 53);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Phone Number (Optional)";
+            this.label2.Text = "📱 Phone Number (Optional)";
             // 
             // btnGoToCusForm
             // 
-            this.btnGoToCusForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(99)))), ((int)(((byte)(235)))));
+            this.btnGoToCusForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
             this.btnGoToCusForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGoToCusForm.ForeColor = System.Drawing.Color.White;
-            this.btnGoToCusForm.Location = new System.Drawing.Point(142, 395);
+            this.btnGoToCusForm.Location = new System.Drawing.Point(115, 530);
             this.btnGoToCusForm.Name = "btnGoToCusForm";
             this.btnGoToCusForm.Size = new System.Drawing.Size(293, 73);
             this.btnGoToCusForm.TabIndex = 4;
-            this.btnGoToCusForm.Text = "Start your order now";
+            this.btnGoToCusForm.Text = "Start Your Order";
             this.btnGoToCusForm.UseVisualStyleBackColor = false;
+            this.btnGoToCusForm.Click += new System.EventHandler(this.btnGoToCusForm_Click);
+            this.btnGoToCusForm.MouseEnter += new System.EventHandler(this.btnGoToCusForm_MouseEnter);
+            this.btnGoToCusForm.MouseLeave += new System.EventHandler(this.btnGoToCusForm_MouseLeave);
             // 
             // txtPhNum
             // 
             this.txtPhNum.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhNum.Location = new System.Drawing.Point(87, 288);
+            this.txtPhNum.Location = new System.Drawing.Point(44, 410);
             this.txtPhNum.Name = "txtPhNum";
-            this.txtPhNum.Size = new System.Drawing.Size(391, 51);
+            this.txtPhNum.Size = new System.Drawing.Size(432, 51);
             this.txtPhNum.TabIndex = 3;
+            this.txtPhNum.Text = "0123456789";
+            this.txtPhNum.Enter += new System.EventHandler(this.txtPhNum_Enter);
+            this.txtPhNum.Leave += new System.EventHandler(this.txPhNum_Leave);
             // 
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(87, 126);
+            this.txtName.Location = new System.Drawing.Point(44, 240);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(391, 51);
+            this.txtName.Size = new System.Drawing.Size(432, 51);
             this.txtName.TabIndex = 2;
+            this.txtName.Text = "Enter your name";
+            this.txtName.Enter += new System.EventHandler(this.txtName_Enter);
+            this.txtName.Leave += new System.EventHandler(this.txtName_Leave);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Sitka Banner", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Sitka Banner", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(80, 73);
+            this.label1.Location = new System.Drawing.Point(36, 170);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 49);
+            this.label1.Size = new System.Drawing.Size(231, 53);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
+            this.label1.Text = "👤Full Name";
             // 
             // btnBack
             // 
-            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
-            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(1330, 12);
+            this.btnBack.Location = new System.Drawing.Point(1379, 12);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(181, 48);
+            this.btnBack.Size = new System.Drawing.Size(132, 48);
             this.btnBack.TabIndex = 5;
-            this.btnBack.Text = "Back";
+            this.btnBack.Text = "← Back";
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            this.btnBack.MouseEnter += new System.EventHandler(this.btnBack_MouseEnter);
+            this.btnBack.MouseLeave += new System.EventHandler(this.btnBack_MouseLeave);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(615, 223);
+            this.pictureBox1.Location = new System.Drawing.Point(605, 301);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(852, 399);
+            this.pictureBox1.Size = new System.Drawing.Size(908, 427);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -132,39 +172,67 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Sitka Banner", 37.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(623, 96);
+            this.label3.Font = new System.Drawing.Font("Georgia", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label3.Location = new System.Drawing.Point(581, 140);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(837, 91);
+            this.label3.Size = new System.Drawing.Size(904, 58);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Welcome to Jinghab Drink Shop";
+            this.label3.Text = "Welcome to Jinghab Drink House";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Lucida Console", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(668, 710);
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label4.Location = new System.Drawing.Point(808, 771);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(726, 28);
+            this.label4.Size = new System.Drawing.Size(438, 17);
             this.label4.TabIndex = 7;
-            this.label4.Text = "One click order with our drink shop system";
+            this.label4.Text = "Fresh Drinks • Fast Service • Easy Ordering";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label6.Location = new System.Drawing.Point(703, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(689, 45);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Order your favorite drinks quickly and easily.";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(944, -3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(176, 189);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
             // 
             // CustomerLoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1579, 814);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "CustomerLoginForm";
             this.Text = "Customer Login Form";
+            this.Load += new System.EventHandler(this.CustomerLoginForm_Load);
+            this.Shown += new System.EventHandler(this.CustomerLoginForm_Shown);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,5 +250,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }

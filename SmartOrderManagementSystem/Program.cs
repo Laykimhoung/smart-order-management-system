8f51cad438632fc3ea8284a12c95e085fcc41496
Lay Kimhoung <houngjk081@gmail.com>
2026-06-11 19:23:37 +0700

    k

--- SmartOrderManagementSystem/Program.cs
+++ SmartOrderManagementSystem/Program.cs
@@ -20,7 +20,7 @@ namespace SmartOrderManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerDashboard("Walk-in Customer"));
+            Application.Run(new LoginForm());
         }
     }
 }

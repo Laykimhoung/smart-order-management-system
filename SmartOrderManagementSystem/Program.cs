364b43d3121e4004f73a38535941c9a18209862a
naingvannath269-maker <naingvannath269@gmail.com>
2026-06-12 18:19:06 +0700

    gg

--- SmartOrderManagementSystem/Program.cs
+++ SmartOrderManagementSystem/Program.cs
@@ -20,7 +20,7 @@ namespace SmartOrderManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerDashboard("John Doe", "hhfhfgf"));
+            Application.Run(new LoginForm());
         }
     }
 }

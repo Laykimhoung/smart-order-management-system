0b50bbba6e0092a306719c006aeab86c7d30e41f
SonitaMom <sonitamom9@gmail.com>
2026-06-13 19:48:24 +0700

    i

--- SmartOrderManagementSystem/Database/DatabaseConnection.cs
+++ SmartOrderManagementSystem/Database/DatabaseConnection.cs
@@ -12,7 +12,8 @@ namespace SmartOrderManagementSystem.Database
     {
         //Use your own local database connection string here
         private static readonly string connectionString =
-        @"Server=DESKTOP-V7K9TLS\SQL2019;Database=SmartOrderManagementDB;User Id=sa;Password=sa;TrustServerCertificate=True;";
+        @"Server=PROTECT-ME;Database=SmartOrderDB;User Id=sa;Password=sa;TrustServerCertificate=True;";
+
 
 
 
